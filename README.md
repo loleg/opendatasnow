8619deb39338b099975e18b000b875e6503f3d67
Oleg Lavrovsky <oleg@utou.ch>
2014-12-23 23:05:16 +0100

    Added raindrops and banner and things

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 Let it snow (Open Data)!
 ========================
 
-A somewhat whimsical interactive visualisation of new open datasets.
+A somewhat whimsical interactive visualisation of new datasets on the pilot Swiss Open Data portal (via CKAN API).
 
 Credits:
 
-* [Snowflakes in CSS3](http://estelle.github.io/snow/) by [Estelle Weyl](http://standardista.com):
+* Public domain photos from the [Swiss National Library](http://www.nb.admin.ch/aktuelles/03147/04467/04642/index.html?lang=de) via [Wikimedia Commons](https://commons.wikimedia.org/w/index.php?title=Category:Media_contributed_by_the_Swiss_National_Library)
+* [Snowflakes in CSS3](http://estelle.github.io/snow/) by [Estelle Weyl](http://standardista.com)
+* [CSS Drop](http://cssdeck.com/labs/css-drop) by [Marco Barría](https://twitter.com/marco_bf)
+
+Libraries:
+
+* [jQuery](http://jquery.com/)
+* [PACE](http://github.hubspot.com/pace/)
+* [SASS](http://sass-lang.com/)
 
 ![Creative Commons License](http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png)
 
--- res/datasnow.js
+++ res/datasnow.js
@@ -1,11 +1,13 @@
+// Sample data request
 var data = {
   size: 5, // get 5 results
   q: 'title:jones' // query on the title field for 'jones'
 };
 
+// Load the CKAN data
 $.ajax({
-  //url: 'data/recently_changed_packages_activity_list.json',
-  url: 'http://opendata.admin.ch/api/3/action/recently_changed_packages_activity_list',
+  url: 'data/recently_changed_packages_activity_list.json',
+  //url: 'http://opendata.admin.ch/api/3/action/recently_changed_packages_activity_list',
   	//{{endpoint}}/_search,
   	//dataType: 'jsonp',
   success: function(data) {
@@ -18,7 +20,7 @@ $.ajax({
 			'author':  	this.data.package.author,
     	});
     });
-    var infobox = $('#legendbox .title');
+    var infobox = $('#legendbox .message');
     var baseurl = 'http://opendata.admin.ch/de/dataset/';
     flakes.find('i').on('mouseover click', function() {
     	var url = baseurl + $(this).data('id');
@@ -28,9 +30,50 @@ $.ajax({
     		'(' + $(this).data('author') + ')');
     });
 
-    $('#loading').fadeOut();
+    // notify PACE
   },
   error: function(err) {
   	console.log(err.responseText);
   }
 });
+
+// Set up crossfader
+var crossTimer = 8, crossCurrent = 0;
+var crossFade = function() {
+  var images = $('.crossfade img').removeClass('active');
+  $(images[crossCurrent]).addClass('active');
+  crossCurrent = (crossCurrent == images.length-1) ? 0 : ++crossCurrent;
+  setTimeout(crossFade, crossTimer * 1000);
+}
+
+// Set up heavy rain
+var $heavyrain = $('#heavyrain');
+for (var i = 0; i < 50; i++) {
+  setTimeout(function() {
+    var pc = Math.round(Math.random() * 100) + '%';
+    $heavyrain.append('<div class="raindrop" '+
+        'style="left:' + pc + '"></div>');
+  }, i * 100);
+}
+
+// Wait at least this long
+Pace.on('done', function() {
+  setTimeout(function() {
+    $('.flakes').removeClass('hidden');
+    $heavyrain.fadeOut(function() { 
+      crossFade(); // start crossfader
+      $(this).remove(); 
+
+      // Hide the help text
+      setTimeout(function() {
+        $('#helpbox').fadeOut('slow');
+      }, 10 * 1000);
+    });
+  }, 2 * 1000);
+});
+
+// Randomize the greetings order
+var randomDivs = $(".banner h2").get().sort(function(){ 
+  return Math.round(Math.random())-0.5;
+});
+$(randomDivs).appendTo(randomDivs[0].parentNode);
